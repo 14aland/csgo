af7f30776e4df1acd018b062545eb70cddd90782
14aland <133007309+14aland@users.noreply.github.com>
2023-05-11 16:27:51 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,175 +1,3 @@
-controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite.setImage(img`
-        . . . . . . . . . f f . . . . . 
-        . . . . . . . . . c f . . . . . 
-        . . . . . . . . . f f . . . . . 
-        . . . . . . . . f c c f . . . . 
-        . . . . . . . . f c c f . . . . 
-        . . . . . . c c c b b f . . . . 
-        . . . . c c a a a c c f . . . . 
-        . . . c a a b b d a a c . . . . 
-        . . c a d d b b b d d a c . . . 
-        . . c a b b b b b b d a c . . . 
-        . . c a b b b b b b b a c . . . 
-        . . c a d b b b b b b a c . . . 
-        . . c a d b b b b d d a c . . . 
-        . . . c a a d b d a a c . . . . 
-        . . . . c c a a a c c . . . . . 
-        . . . . . . c c c . . . . . . . 
-        `)
-    pause(100)
-    mySprite.setImage(img`
-        . . . . . . . . . . . . . f . . 
-        . . . . . . . . . . . . f f f . 
-        . . . . . . . . . . . f f f . . 
-        . . . . . . . . . f f c f . . . 
-        . . . . . . . . f c c f . . . . 
-        . . . . c c c c d c c f . . . . 
-        . . c c a a a c c d f . . . . . 
-        . c a a d b b a a c . . . . . . 
-        c a d d b b b d d a c . . . . . 
-        c a d b b b b b b a c . . . . . 
-        c a b b b b b b b a c . . . . . 
-        c a b b b b b b d a c . . . . . 
-        c a d d b b b b d a c . . . . . 
-        . c a a d b d a a c . . . . . . 
-        . . c c a a a c c . . . . . . . 
-        . . . . c c c . . . . . . . . . 
-        `)
-    pause(100)
-    mySprite.setImage(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . c c c c c . . . . . . . . 
-        . . c a a a a a c f f f f . . . 
-        . c a d b b d d a c b c c f f f 
-        . c a d b b b d a c b c c f c f 
-        c a d b b b b b d a c f f . . . 
-        c a b b b b b b b a c . . . . . 
-        c a d b b b b b b a c . . . . . 
-        . c a b b b b d a c . . . . . . 
-        . c a d d b b d a c . . . . . . 
-        . . c a a a a a c . . . . . . . 
-        . . . c c c c c . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `)
-    pause(100)
-    mySprite.setImage(img`
-        . . . c c c c c . . . . . . . . 
-        . . c a a a a a c . . . . . . . 
-        . c a d b b d d a c . . . . . . 
-        . c a d b b b d a c . . . . . . 
-        c a d b b b b b d a c . . . . . 
-        c a b b b b b b b a c . . . . . 
-        c a d b b b b b b a c . . . . . 
-        . c a b b b b d a c c . . . . . 
-        . c a d d b b d a c d f . . . . 
-        . . c a a a a a c d c c f . . . 
-        . . . c c c c c . f c c f . . . 
-        . . . . . . . . . . f f c f . . 
-        . . . . . . . . . . . . f f f . 
-        . . . . . . . . . . . . . f f f 
-        . . . . . . . . . . . . . . f . 
-        . . . . . . . . . . . . . . . . 
-        `)
-    pause(100)
-    mySprite.setImage(img`
-        . . . . . . . c c c . . . . . . 
-        . . . . . c c a a a c c . . . . 
-        . . . . c a a d b d a a c . . . 
-        . . . c a d d b b b b d a c . . 
-        . . . c a b b b b b b d a c . . 
-        . . . c a b b b b b b b a c . . 
-        . . . c a d b b b b b b a c . . 
-        . . . c a d d b b b d d a c . . 
-        . . . . c a a d b b a a c . . . 
-        . . . . f c c a a a c c . . . . 
-        . . . . f b b c c c . . . . . . 
-        . . . . f c c f . . . . . . . . 
-        . . . . f c c f . . . . . . . . 
-        . . . . . f f . . . . . . . . . 
-        . . . . . f c . . . . . . . . . 
-        . . . . . f f . . . . . . . . . 
-        `)
-    pause(100)
-    mySprite.setImage(img`
-        . . . . . . . . . c c c . . . . 
-        . . . . . . . c c a a a c c . . 
-        . . . . . . c a a d b d a a c . 
-        . . . . . c a d b b b b d d a c 
-        . . . . . c a d b b b b b b a c 
-        . . . . . c a b b b b b b b a c 
-        . . . . . c a b b b b b b d a c 
-        . . . . . c a d d b b b d d a c 
-        . . . . . . c a a b b d a a c . 
-        . . . . . f d c c a a a c c . . 
-        . . . . f c c d c c c c . . . . 
-        . . . . f c c f . . . . . . . . 
-        . . . f c f f . . . . . . . . . 
-        . . f f f . . . . . . . . . . . 
-        . f f f . . . . . . . . . . . . 
-        . . f . . . . . . . . . . . . . 
-        `)
-    pause(100)
-    mySprite.setImage(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . c c c c c . . . 
-        . . . . . . . c a a a a a c . . 
-        . . . . . . c a d b b d d a c . 
-        . . . . . . c a d b b b b a c . 
-        . . . . . c a b b b b b b d a c 
-        . . . . . c a b b b b b b b a c 
-        . . . f f c a d b b b b b d a c 
-        f c f c c b c a d b b b d a c . 
-        f f f c c b c a d d b b d a c . 
-        . . . f f f f c a a a a a c . . 
-        . . . . . . . . c c c c c . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `)
-    pause(100)
-    mySprite.setImage(img`
-        . . . . . . . . . . . . . . . . 
-        . f . . . . . . . . . . . . . . 
-        f f f . . . . . . . . . . . . . 
-        . f f f . . . . . . . . . . . . 
-        . . f c f f . . . . . . . . . . 
-        . . . f c c f . c c c c c . . . 
-        . . . f c c d c a a a a a c . . 
-        . . . . f d c a d b b d d a c . 
-        . . . . . c c a d b b b b a c . 
-        . . . . . c a b b b b b b d a c 
-        . . . . . c a b b b b b b b a c 
-        . . . . . c a d b b b b b d a c 
-        . . . . . . c a d b b b d a c . 
-        . . . . . . c a d d b b d a c . 
-        . . . . . . . c a a a a a c . . 
-        . . . . . . . . c c c c c . . . 
-        `)
-    pause(100)
-    mySprite.setImage(img`
-        . . . . . . . . . f f . . . . . 
-        . . . . . . . . . c f . . . . . 
-        . . . . . . . . . f f . . . . . 
-        . . . . . . . . f c c f . . . . 
-        . . . . . . . . f c c f . . . . 
-        . . . . . . c c c b b f . . . . 
-        . . . . c c a a a c c f . . . . 
-        . . . c a a b b d a a c . . . . 
-        . . c a d d b b b d d a c . . . 
-        . . c a b b b b b b d a c . . . 
-        . . c a b b b b b b b a c . . . 
-        . . c a d b b b b b b a c . . . 
-        . . c a d b b b b d d a c . . . 
-        . . . c a a d b d a a c . . . . 
-        . . . . c c a a a c c . . . . . 
-        . . . . . . c c c . . . . . . . 
-        `)
-})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (myspriteimage == 1) {
         projectile2 = sprites.createProjectileFromSprite(img`
@@ -251,8 +79,87 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
             . . . . . . . . . . . . . . . . 
             `, mySprite, -100, 0)
     }
+    if (myspriteimage == 5) {
+        projectile2 = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . 2 2 2 . . . 
+            . . . . . . . . . 2 3 3 1 2 . . 
+            . . . . . . . . 3 1 1 1 3 2 . . 
+            . . . . . . . 3 1 1 1 1 3 2 . . 
+            . . . . . . . 2 1 1 1 1 2 . . . 
+            . . . . . . 2 1 1 1 1 3 . . . . 
+            . . . . . . 2 1 1 2 3 . . . . . 
+            . . . . . 2 1 2 2 . . . . . . . 
+            . . . . . 1 2 . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite, 100, -100)
+    }
+    if (myspriteimage == 6) {
+        projectile2 = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . 2 2 2 . . . . . . . . . . 
+            . . 2 1 3 3 2 . . . . . . . . . 
+            . . 2 3 1 1 1 3 . . . . . . . . 
+            . . 2 3 1 1 1 1 3 . . . . . . . 
+            . . . 2 1 1 1 1 2 . . . . . . . 
+            . . . . 3 1 1 1 1 2 . . . . . . 
+            . . . . . 3 2 1 1 2 . . . . . . 
+            . . . . . . . 2 2 1 2 . . . . . 
+            . . . . . . . . . 2 1 . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite, -100, -100)
+    }
+    if (myspriteimage == 7) {
+        projectile2 = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . 1 2 . . . . . . . . . 
+            . . . . . 2 1 2 2 . . . . . . . 
+            . . . . . . 2 1 1 2 3 . . . . . 
+            . . . . . . 2 1 1 1 1 3 . . . . 
+            . . . . . . . 2 1 1 1 1 2 . . . 
+            . . . . . . . 3 1 1 1 1 3 2 . . 
+            . . . . . . . . 3 1 1 1 3 2 . . 
+            . . . . . . . . . 2 3 3 1 2 . . 
+            . . . . . . . . . . 2 2 2 . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite, 100, 100)
+    }
+    if (myspriteimage == 8) {
+        projectile2 = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . 2 1 . . . . . 
+            . . . . . . . 2 2 1 2 . . . . . 
+            . . . . . 3 2 1 1 2 . . . . . . 
+            . . . . 3 1 1 1 1 2 . . . . . . 
+            . . . 2 1 1 1 1 2 . . . . . . . 
+            . . 2 3 1 1 1 1 3 . . . . . . . 
+            . . 2 3 1 1 1 3 . . . . . . . . 
+            . . 2 1 3 3 2 . . . . . . . . . 
+            . . . 2 2 2 . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite, -100, 100)
+    }
 })
-let mySpriteretning = 0
 let projectilegone = 0
 let myspriteimage = 0
 let mySprite: Sprite = null
@@ -474,15 +381,170 @@ forever(function () {
 forever(function () {
     if (controller.up.isPressed()) {
         myspriteimage = 1
+        mySprite.setImage(img`
+            . . . . . . . . . f f . . . . . 
+            . . . . . . . . . c f . . . . . 
+            . . . . . . . . . f f . . . . . 
+            . . . . . . . . f c c f . . . . 
+            . . . . . . . . f c c f . . . . 
+            . . . . . . c c c b b f . . . . 
+            . . . . c c a a a c c f . . . . 
+            . . . c a a b b d a a c . . . . 
+            . . c a d d b b b d d a c . . . 
+            . . c a b b b b b b d a c . . . 
+            . . c a b b b b b b b a c . . . 
+            . . c a d b b b b b b a c . . . 
+            . . c a d b b b b d d a c . . . 
+            . . . c a a d b d a a c . . . . 
+            . . . . c c a a a c c . . . . . 
+            . . . . . . c c c . . . . . . . 
+            `)
     }
     if (controller.down.isPressed()) {
         myspriteimage = 2
+        mySprite.setImage(img`
+            . . . . . . . c c c . . . . . . 
+            . . . . . c c a a a c c . . . . 
+            . . . . c a a d b d a a c . . . 
+            . . . c a d d b b b b d a c . . 
+            . . . c a b b b b b b d a c . . 
+            . . . c a b b b b b b b a c . . 
+            . . . c a d b b b b b b a c . . 
+            . . . c a d d b b b d d a c . . 
+            . . . . c a a d b b a a c . . . 
+            . . . . f c c a a a c c . . . . 
+            . . . . f b b c c c . . . . . . 
+            . . . . f c c f . . . . . . . . 
+            . . . . f c c f . . . . . . . . 
+            . . . . . f f . . . . . . . . . 
+            . . . . . f c . . . . . . . . . 
+            . . . . . f f . . . . . . . . . 
+            `)
     }
     if (controller.right.isPressed()) {
         myspriteimage = 3
-        mySpriteretning += 1
+        mySprite.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . c c c c c . . . . . . . . 
+            . . c a a a a a c f f f f . . . 
+            . c a d b b d d a c b c c f f f 
+            . c a d b b b d a c b c c f c f 
+            c a d b b b b b d a c f f . . . 
+            c a b b b b b b b a c . . . . . 
+            c a d b b b b b b a c . . . . . 
+            . c a b b b b d a c . . . . . . 
+            . c a d d b b d a c . . . . . . 
+            . . c a a a a a c . . . . . . . 
+            . . . c c c c c . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
     }
     if (controller.left.isPressed()) {
         myspriteimage = 4
+        mySprite.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . c c c c c . . . 
+            . . . . . . . c a a a a a c . . 
+            . . . . . . c a d b b d d a c . 
+            . . . . . . c a d b b b b a c . 
+            . . . . . c a b b b b b b d a c 
+            . . . . . c a b b b b b b b a c 
+            . . . f f c a d b b b b b d a c 
+            f c f c c b c a d b b b d a c . 
+            f f f c c b c a d d b b d a c . 
+            . . . f f f f c a a a a a c . . 
+            . . . . . . . . c c c c c . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
+    }
+    if (controller.up.isPressed() && controller.right.isPressed()) {
+        myspriteimage = 5
+        mySprite.setImage(img`
+            . . . . . . . . . . . . . f . . 
+            . . . . . . . . . . . . f f f . 
+            . . . . . . . . . . . f f f . . 
+            . . . . . . . . . f f c f . . . 
+            . . . . . . . . f c c f . . . . 
+            . . . . c c c c d c c f . . . . 
+            . . c c a a a c c d f . . . . . 
+            . c a a d b b a a c . . . . . . 
+            c a d d b b b d d a c . . . . . 
+            c a d b b b b b b a c . . . . . 
+            c a b b b b b b b a c . . . . . 
+            c a b b b b b b d a c . . . . . 
+            c a d d b b b b d a c . . . . . 
+            . c a a d b d a a c . . . . . . 
+            . . c c a a a c c . . . . . . . 
+            . . . . c c c . . . . . . . . . 
+            `)
+    }
+    if (controller.up.isPressed() && controller.left.isPressed()) {
+        myspriteimage = 6
+        mySprite.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . f . . . . . . . . . . . . . . 
+            f f f . . . . . . . . . . . . . 
+            . f f f . . . . . . . . . . . . 
+            . . f c f f . . . . . . . . . . 
+            . . . f c c f . c c c c c . . . 
+            . . . f c c d c a a a a a c . . 
+            . . . . f d c a d b b d d a c . 
+            . . . . . c c a d b b b b a c . 
+            . . . . . c a b b b b b b d a c 
+            . . . . . c a b b b b b b b a c 
+            . . . . . c a d b b b b b d a c 
+            . . . . . . c a d b b b d a c . 
+            . . . . . . c a d d b b d a c . 
+            . . . . . . . c a a a a a c . . 
+            . . . . . . . . c c c c c . . . 
+            `)
+    }
+    if (controller.down.isPressed() && controller.right.isPressed()) {
+        myspriteimage = 7
+        mySprite.setImage(img`
+            . . . c c c c c . . . . . . . . 
+            . . c a a a a a c . . . . . . . 
+            . c a d b b d d a c . . . . . . 
+            . c a d b b b d a c . . . . . . 
+            c a d b b b b b d a c . . . . . 
+            c a b b b b b b b a c . . . . . 
+            c a d b b b b b b a c . . . . . 
+            . c a b b b b d a c c . . . . . 
+            . c a d d b b d a c d f . . . . 
+            . . c a a a a a c d c c f . . . 
+            . . . c c c c c . f c c f . . . 
+            . . . . . . . . . . f f c f . . 
+            . . . . . . . . . . . . f f f . 
+            . . . . . . . . . . . . . f f f 
+            . . . . . . . . . . . . . . f . 
+            . . . . . . . . . . . . . . . . 
+            `)
+    }
+    if (controller.down.isPressed() && controller.left.isPressed()) {
+        myspriteimage = 8
+        mySprite.setImage(img`
+            . . . . . . . . . c c c . . . . 
+            . . . . . . . c c a a a c c . . 
+            . . . . . . c a a d b d a a c . 
+            . . . . . c a d b b b b d d a c 
+            . . . . . c a d b b b b b b a c 
+            . . . . . c a b b b b b b b a c 
+            . . . . . c a b b b b b b d a c 
+            . . . . . c a d d b b b d d a c 
+            . . . . . . c a a b b d a a c . 
+            . . . . . f d c c a a a c c . . 
+            . . . . f c c d c c c c . . . . 
+            . . . . f c c f . . . . . . . . 
+            . . . f c f f . . . . . . . . . 
+            . . f f f . . . . . . . . . . . 
+            . f f f . . . . . . . . . . . . 
+            . . f . . . . . . . . . . . . . 
+            `)
     }
 })
